10c5318bf9a97d074e3ad7c957010365c95afb33
Aymeric-k <aymeric.kreicher@gmail.com>
2023-03-26 11:37:31 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { useEffect, useState } from 'react';
+import './App.css';
+import SingleCard from './components/SingleCard';
+const card = [
+ {"src":"./img/alonso.jpg", matched : false},
+ {"src":"./img/antoine.jpg", matched : false},
+ {"src":"/img/benjamin.jpg", matched : false},
+ {"src":"./img/fouad.jpg", matched : false},
+ {"src":"./img/aymeric.jpg", matched : false},
+ {"src":"./img/franck.jpg", matched : false},
+ {"src":"./img/helena.jpg", matched : false},
+ {"src":"./img/jimmy.jpg", matched : false},
+ {"src":"./img/kostandin.jpg", matched : false},
+ {"src":"./img/maxime.jpg", matched : false},
+ {"src":"./img/pierre.jpg", matched : false},
+ {"src":"./img/remyam.jpg", matched : false},
+ {"src":"./img/remyp.jpg", matched : false},
+ {"src":"./img/theo-gamory.jpg", matched : false},
+ {"src":"./img/tristan.jpg", matched : false},
+ {"src":"./img/yoann.jpg", matched : false},
+ {"src":"./img/damienjpg.jpg", matched : false},
+ {"src":"./img/daniel.jpg", matched : false}
+  ]
 function App() {
+  const [cards,setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] =useState(null)
+  const [choiceTwo, setChoiceTwo] =useState(null)
+
+  const shuffleCards = () => {
+    const shuffledCards = [...card, ...card]
+    .sort(()=>Math.random() -0.5)
+    .map((card)=> ({...card, id:Math.random}))
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+  useEffect (() => {
+    if (choiceOne && choiceTwo){
+     if( choiceOne.src === choiceTwo.src){
+       
+       setCards(prevCard  =>{
+        return prevCard.map(card => {
+          if(card.src === choiceOne.src){
+            return {...card, matched: true}
+            
+          }else {
+            return card
+          }
+        })
+       })
+       resetTurn()
+     }else{
+      
+      resetTurn()
+     
+    }
+  }
+  }, [choiceOne, choiceTwo])
+
+
+  const handleChoice = (card) =>{
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+  const resetTurn= () =>{
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns+1)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Jeu de carte Dev-Web 2</h1>
+      <button onClick={shuffleCards}>New Game</button>
+      <div className='card-grid'>
+        {cards.map(card=>(
+          <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
+      ))}
+      </div>
     </div>
   );
 }
